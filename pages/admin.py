f30a290b80c36f4a17655e9b45163e8ca5f67a40
KUMAR NISHANT <kumar.nishant@3i-infotechconsultancy.com>
2022-06-25 00:40:55 +0530

    Team Model completed

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnails(self, objects):
+        return format_html('<img src= "{}" height= "40" style= "border-radius : 50px" />'.format(objects.image.url))
+
+    thumbnails.short_description= 'Photo'
+
+    list_display = ('id', 'thumbnails', 'first_name', 'last_name', 'designation', 'created_date', )
+    list_display_links= ('thumbnails', 'first_name', 'last_name', 'designation', 'created_date', )
+    search_fields= ('first_name', 'last_name', 'designation', )
+    list_filter= ('designation', )
+
+
+admin.site.register(Team, TeamAdmin)
